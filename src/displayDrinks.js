fb666d3dec76ed90e10bef8793a192bb8d90d908
ashan <indrajee79@gmail.com>
2023-02-05 08:35:03 +0530

    js stuff

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -12,5 +12,12 @@ const displayDrinks = ({ drinks }) => {
     }
     const newDrinks = drinks.map((drink) => {
         const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
+
+        return `<a href="drink.html">
+            <article class="cocktail" data-id="${id}">
+                <img src="${image}" alt="${name}">
+                <h3>${name}</h3>
+            </article>
+            </a>`
     })
 }
