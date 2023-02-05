2cbb8bfc3a9bc3a3881d6ad4e496a3191e241bc7
ashan <indrajee79@gmail.com>
2023-02-05 08:30:46 +0530

    js stuff

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -1,3 +1,16 @@
 import get from './getElement.js';
 import { hideLoading } from './toggleLoading.js';
-const displayDrinks
+const displayDrinks = ({ drinks }) => {
+    const section = get('.section-center');
+    const title = get('.title');
+    if (!drinks) {
+        //hide Loading
+        hideLoading();
+        title.textContent = 'Sorry, No Drinks Matched Your Search';
+        section.innerHTML = null;
+        return;
+    }
+    const newDrinks = drinks.map((drink) => {
+        const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
+    })
+}
