a38ab47252347260d54f634cd1fc12da43a5b410
ashan <indrajee79@gmail.com>
2023-02-06 07:57:31 +0530

    js stuff

--- src/fetchDrinks.js
+++ src/fetchDrinks.js
@@ -1,13 +1,13 @@
-import { showLoading } from './toggleLoading.js';
+import { showLoading } from "./toggleLoading.js";
 const fetchDrinks = async (url) => {
-    showLoading();
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
+  showLoading();
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default fetchDrinks;
