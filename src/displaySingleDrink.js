b12c00c289d4b2546b44d9cc630de2848816884f
ashan <indrajee79@gmail.com>
2023-02-06 07:06:03 +0530

    js stuff

---
+++ src/displaySingleDrink.js
@@ -0,0 +1,19 @@
+import { hideLoading } from './toggleLoading.js';
+import get from './getElement.js';
+
+const displayDrink = (data) => {
+    hideLoading();
+
+    const drink = data.drinks[0];
+    const { strDrinkThumb: image, strDrink: name, strInstructions: desc } = drink;
+    const list = [
+        drink.strIngredient1,
+        drink.strIngredient2,
+        drink.strIngredient3,
+        drink.strIngredient4,
+        drink.strIngredient5,
+    ];
+    const img = get('.drink-img');
+    const drinkName =  get('.drink-name');
+    
+}
