7cfcce3b7b38987e3ac960fdfa2c0e2a1a401adf
ashan <indrajee79@gmail.com>
2023-02-06 07:23:37 +0530

    js stuff

---
+++ src/presentDrinks.js
@@ -0,0 +1,13 @@
+import fetchDrinks from "./fetchDrinks.js";
+import displayDrinks from "./displayDrinks.js";
+import setDrink from "./setDrink.js";
+const showDrinks = async (url) => {
+
+    //display drinks
+    const section =  await displayDrinks(data);
+    if (section) {
+        setDrink(section);
+    }
+};
+
+export default showDrinks;
